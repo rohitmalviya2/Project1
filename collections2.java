ba5d60afe71d1835588cc6c252af219e4717d52f
ROHIT <rohit.malviya@spsu.ac.in>
2022-06-15 17:35:49 +0530

    first commit

---
+++ collections2.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+class javacollections{
+
+    public static void main(String[] args) {
+            LinkedList<String> b = new LinkedList<String>();
+            b.addFirst("check");
+            b.add("Rohit");
+            b.add("aadhya");
+            b.add("shivangi");
+            b.add("prakash");
+for(String str:b)
+{
+            System.out.println(str);
+}
+        
+
+    }
+}
