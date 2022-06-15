2c886a7cb2cd8a5356ff6f67b995f2f13b93dd25
ROHIT <rohit.malviya@spsu.ac.in>
2022-06-15 13:58:37 +0530

    first commit

---
+++ Method.java
@@ -0,0 +1,14 @@
+ class Method {
+     public static void main(String[] args){
+         int c = show(10,20);
+         System.out.println(c);
+
+
+     }
+    static int show(int a, int b)
+     {
+         return a+b;
+     }
+
+    
+}
