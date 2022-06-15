59c7b6f8189a4b3f7629212a4bbbfc54f076ecfb
ROHIT <rohit.malviya@spsu.ac.in>
2022-06-15 14:10:49 +0530

    first commit

---
+++ strin.java
@@ -0,0 +1,10 @@
+ class string {
+    public static void main(String[] args) {
+        String str = "AADHYA";
+        String str2 = str.concat("sun");
+        System.out.println(str2);
+        
+        
+    }
+    
+}
